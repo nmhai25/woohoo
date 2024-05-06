89361766664000c698a8148ba7240a33ed3f5ee9
namhaidorjj <mglcoc102@gmail.com>
2024-05-06 11:44:25 +0800

    s

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,13 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const posts = await prisma.post.create({
-    data: {
-      name: "Hello World",
-    },
-  });
-  console.log("object", posts);
-}
+// async function main() {
+//   const posts = await prisma.post.create({
+//     data: {
+//       name: "Hello World",
+//     },
+//   });
+//   console.log("object", posts);
+// }
 
-main();
+// main();
